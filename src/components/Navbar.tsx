5dbaebc7881a8a4399562675605655aab6f9d1e6
Wouter Hoenderdaal <wouter.hoenderdaal@wur.nl>
2025-07-21 10:03:38 +0200

    refactor layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,35 +1,48 @@
-import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
-import LogoutIcon from "@mui/icons-material/Logout";
+import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material'
+import MenuIcon from "@mui/icons-material/Menu";
 import DashboardIcon from "@mui/icons-material/Dashboard";
+import LogoutIcon from "@mui/icons-material/Logout";
 
-const Navbar = () => {
+interface Props {
+  handleDrawerToggle: () => void;
+}
+
+const Navbar = ({ handleDrawerToggle }: Props) => {
   return (
     <AppBar
-      position="fixed"
-      sx={{
-        zIndex: (theme) => theme.zIndex.drawer + 1,
-      }}
-      color="primary" 
-    >
-      <Toolbar sx={{ justifyContent: "space-between" }}>
-        <Box>
-          <DashboardIcon
-            fontSize="large"
-            sx={{
-              display: { xs: "none", sm: "block" },
-              width: "8rem",
-            }}
-          />
-        </Box>
-        <Typography variant="h6">Title</Typography>
-        <Box>
-          <IconButton>
-            <LogoutIcon />
-          </IconButton>
-        </Box>
-      </Toolbar>
-    </AppBar>
-  );
-};
+        position="fixed"
+        sx={{
+          zIndex: (theme) => theme.zIndex.drawer + 1,
+        }}
+      >
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <Box>
+            <DashboardIcon
+              fontSize="large"
+              sx={{
+                display: { xs: "none", sm: "block" },
+                width: "8rem",
+              }}
+            />
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: "none" } }}
+            >
+              <MenuIcon fontSize="large" />
+            </IconButton>
+          </Box>
+          <Typography variant="h6">Title</Typography>
+          <Box>
+            <IconButton>
+              <LogoutIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+  )
+}
 
-export default Navbar;
+export default Navbar
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,98 @@
+import { Box, Drawer, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar } from "@mui/material";
+import MailIcon from "@mui/icons-material/Mail";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+
+export const drawerWidth = 240;
+
+interface Props {
+  window?: () => Window;
+  mobileOpen: boolean;
+  handleDrawerTransitionEnd: () => void;
+  handleDrawerClose: () => void;
+}
+
+
+const Sidebar = ({ window, mobileOpen, handleDrawerTransitionEnd, handleDrawerClose }: Props) => {
+
+  const container =
+  window !== undefined ? () => window().document.body : undefined;
+
+  const drawer = (
+    <div>
+      <Toolbar />
+      <Divider />
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  )
+
+  return (
+    <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        <Toolbar />
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onTransitionEnd={handleDrawerTransitionEnd}
+          onClose={handleDrawerClose}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+          slotProps={{
+            root: {
+              keepMounted: true, // Better open performance on mobile.
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+  )
+}
+
+export default Sidebar
