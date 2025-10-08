0a88fd4755746d7862785b0be97df8e5f1b641e1
Wouter Hoenderdaal <wouter.hoenderdaal@wur.nl>
2025-10-08 13:29:02 +0200

    add sop param to download excel

--- src/components/DownloadExcelButton.tsx
+++ src/components/DownloadExcelButton.tsx
@@ -3,36 +3,46 @@ import SaveIcon from "@mui/icons-material/Save";
 import useTheme from "../hooks/useTheme";
 import { themeSettings } from "../themes/theme";
 import useData from "../hooks/useData";
+import axios from "axios";
+import useAuth from "../hooks/useAuth";
 
 const DownloadExcelButton = () => {
   const { mode, accentColor } = useTheme();
   const themeColors = themeSettings(mode, accentColor);
-  
-  const { data } = useData();
+
+  // get user authentication data
+  const { auth } = useAuth();
+
+  // get selected SOP
+  const { sop } = useData();
 
   const handleDownload = async () => {
+    const controller = new AbortController();
     try {
-      // payload
-      const payload = data;
-
       // Call the FastAPI endpoint
-      const response = await fetch(
+      const response = await axios(
         "http://127.0.0.1:8000/mcdata/download/",
         {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(payload),
+          method: "GET",
+          responseType: "blob",
+          signal: controller.signal,
+          headers: { Authorization: "Bearer " + auth.accessToken },
+          params: {
+            sop: sop
+          }
         }
       );
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
+      //if (!response.ok) {
+      //  throw new Error(`HTTP error! Status: ${response.status}`);
+      //}
 
       // Convert response to a blob
-      const blob = await response.blob();
+      //const blob = await response.blob();
+          // Create a blob URL and trigger download
+      const blob = new Blob([response.data], {
+        type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      });
 
       // Create a temporary URL for the blob
       const url = window.URL.createObjectURL(blob);
